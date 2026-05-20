111f56901067fe6e92b44a992ca624d54d35305b
Mariana Madrigal <mariana.madrigal.molina@est.una.ac.cr>
2026-05-20 16:11:29 -0600

    mismo servidor

--- Backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/publico/**").permitAll()
                         .requestMatchers("/error").permitAll()
+                        .requestMatchers("/", "/index.html", "/assets/**", "/*.svg", "/*.js", "/*.css").permitAll()
 
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/empresa/**").hasRole("EMPRESA")
